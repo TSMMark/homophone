534cba4d5162256ee56345e4c98921b3939352e3
TSMMark <marka@trendsettermarketing.net>
2014-03-31 00:08:22 -0400

    add pages helper

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,17 @@
+module PagesHelper
+  def full_page_title
+    "Homophone.com " +
+      (content_for?(:title) ? "| #{content_for(:title)}" : "- Your Complete List of Homophones")
+  end
+
+  def page_h
+    "h3"
+  end
+
+  def btn_view_more options={}
+    defaults = {
+      class: "btn btn-primary"
+    }
+    link_to "Click for more homophones !", word_sets_path, defaults.merge(options)
+  end
+end
