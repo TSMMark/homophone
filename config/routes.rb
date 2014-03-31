e629a85d20d585ceb1bf63a13862c9582305afee
TSMMark <marka@trendsettermarketing.net>
2014-03-31 00:06:17 -0400

    SEO updates

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources :words
 
   root to: "pages#home"
+
+  get '/index' => redirect('/')
+
+  get '/about/index' => redirect('/about')
+  get '/word_sets/index' => redirect('/word_sets')
+
 end
