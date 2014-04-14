68d614418cbdf77d2b0d462f22a0d41363dd9e86
TSMMark <marka@trendsettermarketing.net>
2014-04-14 09:40:47 -0400

    delete test directory

--- test/helpers/word_sets_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class WordSetsHelperTest < ActionView::TestCase
-end
--- test/models/definition_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DefinitionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
