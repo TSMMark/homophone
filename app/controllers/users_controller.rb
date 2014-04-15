e0a815a1bd51e852eae36f51e90fcdcc028fa0e4
TSMMark <marka@trendsettermarketing.net>
2014-04-14 21:15:13 -0400

    authorize user update and validate new password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+
+  load_and_authorize_resource only: [:edit, :update]
 
   # GET /users/1/edit
   def edit
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'User was successfully updated.' }
         format.json { render action: 'show', status: :ok, location: @user }
       else
         format.html { render action: 'edit' }
@@ -19,15 +20,10 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
 
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :email
   validates_presence_of :email
+
+  validates :password, presence: true,
+            length: { minimum: 5, maximum: 40 },
+            confirmation: true
+
+  validates_confirmation_of :password
 end
