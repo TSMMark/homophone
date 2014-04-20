d6dce68bb1f8761f2186fb4c0d86a43a3ec020f0
TSMMark <marka@trendsettermarketing.net>
2014-04-20 19:33:31 -0400

    hotfix email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email].downcase)
 
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
